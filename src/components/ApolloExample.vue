cb0e5aef396ecf2b6146c1bb1642e2649e0f04b8
Zelig880 <scuomo1988@gmail.com>
2019-02-12 23:52:18 +0000

    add some style

--- src/components/ApolloExample.vue
+++ src/components/ApolloExample.vue
@@ -11,21 +11,41 @@
       
       <button @click="triggerQuery()" @disabled="$apollo.queries.search.loading" >Search</button>
     </div>
-    <div> Currently showing page number: {{ currentPage }} </div>
-    <div v-if="$apollo.loading">Loading...</div>
-    <div v-if="showResult">
-          Total user found: {{ search.userCount }}
-          <div v-for="(node, key) in search.nodes" :key="key" v-if="node.__typename === 'User'"> 
-            1: {{node.name}},
-            2: {{node.avatarUrl}},
-            3: {{node.bio}},
-            6: {{node.starredRepositories.totalCount}},
-            7: {{node.url}}
-          </div>
+
+    <div v-if="showResult" class="searchStats">
+      <button @click="previousPage()" :disabled="!previousPageAvailable">Previous page</button>
+      <span>Current Page: {{ currentPage }}</span>
+      <span>Total Page: {{ totalPages }}</span>
+      <span>Total Users: {{ search.userCount }}</span>
+      <button @click="nextPage()" :disabled="!nextPageAvailable" >Next page</button>
     </div>
 
-    <button @click="nextPage()" :disabled="!nextPageAvailable" >Next page</button>
-    <button @click="previousPage()" :disabled="!previousPageAvailable">Previous page</button>
+    <div v-if="$apollo.loading">Loading...</div>
+    <div v-if="showResult">
+      <div> Currently showing page number: {{ currentPage }} </div>
+      <div id="userContainer">
+        <div 
+          class="user" 
+          v-for="(node, key) in search.nodes" 
+          :key="key" 
+          v-if="node.__typename === 'User'"
+        > 
+          <img class="avatar" :src="node.avatarUrl" :alt="node.name + 'avatar Image'" />
+          <h2>{{node.name}}</h2>
+          <h3>{{node.bio}}</h3>
+          <p>
+            FOLLOWING:  {{node.starredRepositories.totalCount}}<br/>
+            FOLLOWERS:  {{node.followers.totalCount}}<br/>
+            STARRED REPOs: {{node.following.totalCount}}
+          </p>
+          <a :href="node.url" target="_blank">Go to Profile</a>
+        </div>
+      </div>
+      
+      <button @click="previousPage()" :disabled="!previousPageAvailable">Previous page</button>
+      <button @click="nextPage()" :disabled="!nextPageAvailable" >Next page</button>
+    </div>
+
     
   </div>
 </template>
@@ -64,24 +84,30 @@ export default {
   computed: {
     nextPageAvailable(){
 
-      if(!this.search.hasOwnProperty("pageInfo")) return false;
+      if(!this.searchCompleted()) return false;
 
       return this.search.pageInfo.hasNextPage;
     },
     previousPageAvailable(){
-      if(!this.search.hasOwnProperty("pageInfo")) return false;
+      if(!this.searchCompleted()) return false;
 
       return this.currentPage > 1;
     },
     showResult(){
-      var queryIsEmpty = this.search.hasOwnProperty("userCount");
       var graphQlLoading = this.$apollo.loading;
 
-      if (!queryIsEmpty || graphQlLoading){
+      if (!this.searchCompleted() || graphQlLoading){
         return false;
       }else{
         return true;
       }
+    },
+    totalPages(){
+      if(!this.searchCompleted()) return 0;
+      
+      var totalPages = Math.ceil(this.search.userCount / 10);
+
+      return totalPages;
     }
   },
   methods: {
@@ -103,6 +129,9 @@ export default {
       this.userInput = this.$refs.userInput.value;
       this.queryDisabled = false;
       this.currentPage= 1;
+    },
+    searchCompleted(){
+      return this.search.hasOwnProperty("pageInfo");
     }
   },
   beforeMount(){
@@ -111,51 +140,70 @@ export default {
 }
 </script>
 
-<style scoped>
-.form,
-.input,
-.apollo,
-.message {
-  padding: 12px;
-}
+<style lang="scss" scoped>
+.form{
 
-label {
-  display: block;
-  margin-bottom: 6px;
+  width: 400px;
+  margin: auto;
+  label, input, button{
+    display: block;
+    width: 80%;
+    margin: auto;
+    padding: 10px;
+    margin-bottom: 15px;
+  }
+  label{
+    font-weight: bold;
+    font-size: 1.25rem;
+  }
+  input{
+    width: 75%;
+  }
 }
-
-.input {
-  font-family: inherit;
-  font-size: inherit;
-  border: solid 2px #ccc;
-  border-radius: 3px;
-}
-
-.error {
-  color: red;
-}
-
-.images {
-  display: grid;
-  grid-template-columns: repeat(auto-fill, 300px);
-  grid-auto-rows: 300px;
-  grid-gap: 10px;
-}
-
-.image-item {
+.searchStats{
   display: flex;
-  align-items: center;
-  justify-content: center;
-  background: #ccc;
-  border-radius: 8px;
+  justify-content: space-around;
+  align-content: space-around;
+  height: 50px;
+  margin:10px;
+  align-items:baseline;
+  
+  span, button {
+    justify-content: space-around;
+    width: 13%;
+    height: 40px;
+  }
+  span{
+    border-bottom: 2px solid #999999;
+  }
 }
 
-.image {
-  max-width: 100%;
-  max-height: 100%;
-}
+#userContainer {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-content: space-around;
+  height: auto;
+  margin:10px;
+  
+  .user {
+    background-color: white;
+    box-shadow: 7px 10px lightgrey;
+    flex-grow: 1;
+    justify-content: space-between;
+    width: 25%;
+    min-width: 350px;
+    max-width: 400px;
+    margin:10px 7px
+  }
 
-.image-input {
-  margin: 20px;
+  .avatar{
+    margin-top: 10px;
+    margin-left: 10px;
+    width:150px;
+    border-radius: 50%;
+    float:left;
+    height: auto;
+  }
 }
 </style>
